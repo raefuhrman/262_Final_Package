d536ecc66028fe0d2622f5617b492a43cebcf9eb
cristinasparks <csparks@bren.ucsb.edu>
2019-06-07 12:53:05 -0700

    updates

--- test_script_cs.R
+++ test_script_cs.R
@@ -3,58 +3,56 @@ library(tidyverse)
 library(reshape2)
 library(lubridate) # Can this be in function R file
 
-climate_sb_2018 <- read.csv(("Climate_Data_SB_2018.csv"))
-climate_sb_2018$DATE <- parse_date_time(x = climate_sb_2018$DATE,
+climate_data <- read.csv(("Climate_Data_SB_2018.csv"))
+climate_data$DATE <- parse_date_time(x = climate_data$DATE,
                 orders = c("d m y", "d B Y", "mdy"))
-climate_sb_2018$DATE <- as.Date(climate_sb_2018$DATE, format = "%m/%d/%y")
+climate_data$DATE <- as.Date(climate_sb_2018$DATE, format = "%m/%d/%y")
 
 
-climate_sb_2018_modified <- climate_sb_2018 %>% 
-  mutate(month = format(climate_sb_2018_modified$DATE, "%m"), year = format(climate_sb_2018_modified$DATE, "%Y"))
+climate_sb_2018_modified <- climate_sb_2018 %>%
+  mutate(month = format(climate_sb_2018_modified$DATE, "%m"), year = format(climate_sb_2018_modified$DATE, "%Y")) %>%
+  filter(STATION == "US1CASB0009")
 
-avg_precip <- climate_sb_2018_modified %>% 
-  filter(STATION == "US1CASB0009") %>% 
-  select(month,year, PRCP) %>% 
-  group_by(month, year) %>% 
+avg_precip <- climate_sb_2018_modified %>%
+  filter(STATION == "US1CASB0009") %>%
+  select(month,year, PRCP) %>%
+  group_by(month, year) %>%
   summarize(average = mean(PRCP))
 
+avg_month_precip = function(climate = climate_sb_2018_modified) {
+  month_precip <- climate %>%
+    select(month, year, PRCP) %>%
+    group_by(month, year) %>%
+    summarize(average = mean(PRCP))
 
+  return(month_precip)
 
-
-
-
-
-plant_growth = function(PRCP) {
-  pg = (PRCP) ** 3 + 5
-  
-  if (PRCP <= 0) {
-    pg = 0
-  }
-  
-  if(PRCP > 1) {
-    pg = (PRCP) ** 2 + 1
-  }
-  
-  return (pg)
-  
 }
 
+avg_month_precip()
 
-leafgrowth = function(t) {
-  lf = (t)**2 + 20
-  
-  if (t > 30) {
-    lf = lf / 10
+
+sunshine_hours = array(dim=c(6,12,3)) # Years, Months, Locations
+
+for (i in 1:6) {
+  for (j in 1:12) {
+    value = runif(min=0,max=10,n=3)
+    sunshine_hours[i,j,]=value
   }
-  
-  if (t < 0) {
-    lf = 0
-  }
-  
-  return(lf)
 }
 
+dimnames(sunshine_hours) = list(c("2001","2002","2003","2004","2005", "2006"),
+                       c("Jan","Feb","Mar","Apr", "May", "Jun", "Jul", "Aug",
+                         "Sep", "Oct", "Nov", "Dec"),
+                       c("Loc1","Loc2", "Loc3"))
 
-#HOW do you convert dataframe into multidimensional? Example was just populating a blank thing
+
+plant_growth = function(sun = sunshine_hours) {
+
+    growth = ifelse (sunshine_hours > 1, sunshine_hours^2 + 3, 0)
+    return(growth)
+
+}
+
 
 
