edded665ce64cc1fa54b5bbd2cc47103d7969fe4
cristinasparks <csparks@bren.ucsb.edu>
2019-06-08 18:47:04 -0700

    adding data and data documentation for imaginary sunshine hours

---
+++ R/sunshine_hours.R
@@ -0,0 +1,10 @@
+#' Imaginary sunshine hours
+#'
+#' Imaginary data for sunshine hours by month and year for three imaginary locations
+#'
+#'  @format A multidimensional array, with years in rows, months in columns, and locations in the third dimension.
+#'  @source Random numbers generated between 0 and 10 for each year-month-location combination (it's not a very sunny place)
+#'  \itemize{
+#'  \item sunshine hours
+#'  }
+#'
