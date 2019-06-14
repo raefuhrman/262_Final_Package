b287223508cb233e6d3b6e52cd72bde467eb3596
cristinasparks <csparks@bren.ucsb.edu>
2019-06-13 21:52:09 -0700

    fixing issue with documentation

--- R/plant_growth_mda.R
+++ R/plant_growth_mda.R
@@ -2,9 +2,7 @@
 #'
 #' Imaginary data for monthly plant growth, in mm, by months and years between 2001 and 2006 for three locations.  Based off of sunshine_hours and the Plant_Growth function used to generate growth from sunshine hours.
 #'
-#'  @format A multidimensional array, with years in rows, months in columns, and locations in the third dimension.
-#'
-#'  @source Plant_Growth function applied to sunshine_hours (random data)
+#'  This data is a multidimensional array, with years in rows, months in columns, and locations in the third dimension.  It was creating using the Plant_Growth function applied to sunshine_hours (random data)
 #'
 #'  \itemize{
 #'  \item plant growth (mm)
--- R/sunshine_hours.R
+++ R/sunshine_hours.R
@@ -2,8 +2,7 @@
 #'
 #' Imaginary data for average daily sunshine hours by month and year (2001-2006) for three imaginary locations
 #'
-#'  @format A multidimensional array, with years in rows, months in columns, and locations in the third dimension.
-#'  @source Random numbers generated between 0 and 10 for each year-month-location
+#'  This data is a multidimensional array, with years in rows, months in columns, and locations in the third dimension.  It was created from random numbers generated between 0 and 10 for each year-month-location
 #'  \itemize{
 #'  \item sunshine hours
 #'  }
