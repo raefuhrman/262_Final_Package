484a4454bd374bb152597943b607bbc054bb67ef
Rae Fuhrman <r_fuhrman@ucsb.edu>
2019-06-13 17:45:20 -0700

    test that isnt working

--- tests/testthat/test-average_monthly_temperature.R
+++ tests/testthat/test-average_monthly_temperature.R
@@ -1,5 +1,5 @@
 context("test-average_monthly_temperature")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("monthly average temperature is above freezing in Santa Barbara", {
+  expect_that(min(!is.na(average_temp))>=32)
 })
